2f42eecc33356dc5c029d9fed20fd5f878b7b284
jasurbek <jasurdev1604@gmail.com>
2025-07-30 16:02:42 +0500

    main pahe

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ let mobile = document.querySelector(".nav-mobile");
 let body = document.querySelector("body");
 
 function openMenu() {
-  body.style.overflowY = "hidden";
   bg.classList.add("run");
   mobile.classList.add("run-mobile");
 }
 
 function closeBg() {
-  body.style.overflowY = "auto";
   bg.classList.remove("run");
   mobile.classList.remove("run-mobile");
 }
